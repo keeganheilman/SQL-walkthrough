bb58d4a8d4f970199e109c87f9be16eefb7154fa
Keegan Heilman <keegan.heilman@gmail.com>
2022-02-19 15:36:30 -0800

    insert, update, delete statements

--- chapter_2/table_person_insert.sql
+++ chapter_2/table_person_insert.sql
@@ -1,3 +1,9 @@
 
-INSERT INTO person(person_id, fname, lname, eye_color, birth_date) VALUES (null, 'William','Turner', 'BR', '1972-05-27');
+-- INSERT a row to `person` table
+INSERT INTO person(fname, lname, eye_color, birth_date) VALUES ('William','Turner', 'BR', '1972-05-27');
 -- postgres will convert the string to a date given it is provided in the required format.
+
+
+-- INSERT another row to `person` table
+INSERT INTO person(fname, lname, eye_color, birth_date, street, city, state, country, postal_code)
+VALUES ('Susan', 'Smith', 'BL', '1975-11-02', '23 Maple St.', 'Arlington', 'VA', 'USA', '20220');
