0008ca185e8f06896e7c7613155da8e1e113c9e9
Keegan Heilman <keegan.heilman@gmail.com>
2022-02-19 14:48:11 -0800

    create insert statements

---
+++ chapter_2/table_fav_food_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO favorite_food (person_id, food) VALUES (1, 'pizza');
+INSERT INTO favorite_food (person_id, food) VALUES (1, 'cookies');
+INSERT INTO favorite_food (person_id, food) VALUES (1, 'nachos');
---
+++ chapter_2/table_person_insert.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO person(person_id, fname, lname, eye_color, birth_date) VALUES (null, 'William','Turner', 'BR', '1972-05-27');
+-- postgres will convert the string to a date given it is provided in the required format.
