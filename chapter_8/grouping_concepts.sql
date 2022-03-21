95f75677cc3d42a6d8645d562078e5d11618c17f
Keegan Heilman <keegan.heilman@gmail.com>
2022-03-21 11:40:43 -0700

    starting chapter8

---
+++ chapter_8/grouping_concepts.sql
@@ -0,0 +1,20 @@
+-- grouping concepts
+-- group the data using the `GROUP BY` clause
+-- the following provides one row for each distinct value in `customer_id` column
+SELECT customer_id
+FROM rental
+GROUP BY customer_id;
+
+-- you can then aggregate  using and `aggregate function` in the select clause to count the number of rows in each group
+-- count() counts the number of rows in each group, and the asterisk tells the server to count evertthing in the group
+SELECT customer_id, count(*)
+FROM rental
+GROUP BY customer_id;
+
+-- further
+SELECT customer_id, count(*)
+FROM rental
+GROUP BY customer_id
+ORDER BY 2 DESC; -- orders the resulting data by the second column in descending order
+
+
