cb961800b72d11df9ded653c001c40d0020fde71
Keegan Heilman <keegan.heilman@gmail.com>
2022-04-09 17:20:14 -0700

    end of chapter9 examples

--- chapter_9/subqueries.sql
+++ chapter_9/subqueries.sql
@@ -110,3 +110,114 @@ WHERE (actor.id, film_id) IN (
     WHERE a.last_name = 'MONROE'
     AND f.rating = 'PG'
 );
+
+-- correlated subqueries
+-- NOTE: the subquery is ran for each row returned by the containing query
+-- here, subquery is ran 599 times, once for each c.customer_id provided by the containing query
+SELECT c.first_name, c.last_name
+FROM customer c
+WHERE 20 = (
+    SELECT count(*)
+    FROM rental r
+    WHERE r.customer_id = c.customer_id
+);
+
+
+-- combine correlated subqueries and range condition
+SELECT c.first_name, c.last_name
+FROM customer c
+WHERE (
+    SELECT sum(p.amount) FROM payment p
+    WHERE p.customer_id = c.customer_id
+) BETWEEN 180 AND 240;
+
+-- exists Operator
+-- exist operator used when want to identify that a relationship exists without regard for the quantity.
+-- convention is to specify either SELECT 1 or SELECT * when using exists
+SELECT c.first_name, c.last_name
+FROM customer c
+WHERE EXISTS (
+    SELECT 1 FROM rental r
+    WHERE r.customer_id = c.customer_id
+    AND r.rental_date::date < '2005-05-25'
+);
+
+-- using not exists
+SELECT a.first_name, a.last_name
+FROM actor a
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM film_actor fa
+    INNER JOIN film f ON f.film_id = fa.film_id
+    WHERE fa.actor_id = a.actor_id
+    AND f.rating = 'R'
+);
+
+UPDATE customer AS c
+SET last_update = (
+    SELECT max(r.rental_date) 
+    FROM rental r
+    WHERE r.customer_id = c.customer_id
+);
+
+
+
+-- when to use subqueries
+
+-- subqueries as data sources
+SELECT c.first_name, c.last_name, pymnt.num_rentals, pymnt.tot_payments
+FROM customer c
+    INNER JOIN (
+        SELECT customer_id,
+        count(*) num_rentals, sum(amount) tot_payments
+        FROM payment
+        GROUP BY customer_id
+    ) pymnt
+    ON c.customer_id = pymnt.customer_id;
+
+
+-- data generate, to then use as a subquery
+SELECT 'Small Fry' name, 0 low_limit, 74.99 high_limit
+UNION ALL
+SELECT 'Average Joes' name, 75 low_limit, 149.99 high_limit
+UNION ALL
+SELECT 'Heavy Hitters' name, 150 low_limit, 9999999.99 high_limit;
+
+SELECT pymnt_grps.name, count(*) num_customers
+FROM (
+    SELECT customer_id,
+    count(*) num_rentals, sum(amount) tot_payments
+    FROM payment
+    GROUP BY customer_id
+) pymnt
+INNER JOIN (
+    SELECT 'Small Fry' name, 0 low_limit, 74.99 high_limit
+    UNION ALL
+    SELECT 'Average Joes' name, 75 low_limit, 149.99 high_limit
+    UNION ALL
+    SELECT 'Heavy Hitters' name, 150 low_limit, 9999999.99 high_limit
+) pymnt_grps
+ON pymnt.tot_payments
+BETWEEN pymnt_grps.low_limit AND pymnt_grps.high_limit
+GROUP BY pymnt_grps.name;
+
+--task-oriented subqueries
+-- such as seperating out the grouping mechanism
+SELECT customer_id, count(*) tot_rentals, sum(amount) tot_payments
+FROM payment
+GROUP BY customer_id;
+
+
+SELECT c.first_name, c.last_name, ct.city, pymnt.tot_payments, pymnt.tot_rentals
+FROM (
+    SELECT customer_id, count(*) tot_rentals, sum(amount) tot_payments
+FROM payment
+GROUP BY customer_id
+) pymnt
+INNER JOIN customer c
+ON pymnt.customer_id = c.customer_id
+INNER JOIN address a
+ON c.address_id = a.address_id
+INNER JOIN city ct
+ON a.city_id = ct.city_id;
+
