39cafb20bdf75ec5108d13ef2fe9b65b8e143212
Keegan Heilman <keegan.heilman@gmail.com>
2022-05-01 10:13:35 -0700

    complete problem: 15 days of learning SQL

--- practice/15_days_of_learning_SQL.sql
+++ practice/15_days_of_learning_SQL.sql
@@ -6,16 +6,34 @@ Enter your query here.
 */
 
 
-select 
-submission_date ,
-
-( SELECT COUNT(distinct hacker_id)  
- FROM Submissions s2  
- WHERE s2.submission_date = s1.submission_date AND    (SELECT COUNT(distinct s3.submission_date) FROM      Submissions s3 WHERE s3.hacker_id = s2.hacker_id AND s3.submission_date < s1.submission_date) = dateDIFF(s1.submission_date , '2016-03-01')) ,
-
-(select hacker_id  from submissions s2 where s2.submission_date = s1.submission_date 
-group by hacker_id order by count(submission_id) desc , hacker_id limit 1) as shit,
-(select name from hackers where hacker_id = shit)
-from 
-(select distinct submission_date from submissions) s1
-group by submission_date
+SELECT
+    submission_date,
+    (
+        SELECT COUNT(DISTINCT hacker_id)
+        FROM Submissions s2 
+        WHERE s2.submission_date = s1.submission_date
+        AND DATEDIFF(s1.submission_date , '2016-03-01') = 
+            (
+                SELECT COUNT(DISTINCT s3.submission_date)
+                FROM submissions s3
+                WHERE s3.hacker_id = s2.hacker_id
+                AND s3.submission_date < s1.submission_date
+            )
+    ) sub_counts, 
+    (
+        SELECT hacker_id
+        FROM submissions s2
+        WHERE s2.submission_date = s1.submission_date 
+        GROUP BY hacker_id
+        ORDER BY count(submission_id) DESC, hacker_id limit 1
+    ) h_id,
+    (
+        SELECT h.name
+        FROM hackers h
+        WHERE h.hacker_id = h_id
+    ) h_name
+FROM (
+    DISTINCT submission_date
+    FROM submissions
+    ) s1
+GROUP BY submission_date
